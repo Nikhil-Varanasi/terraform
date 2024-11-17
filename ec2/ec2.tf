9fbaae96f5095c1ddc2bc0220cb197ad4e07f96f
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2024-11-17 13:18:53 +0530

    adding sg fie

---
+++ ec2/ec2.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allow_ssh_terraform" {
+    name        = "Allow-ssh"
+    description = "Allow port 22 for ssh access"
+
+    ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    tags = {
+    Name = "allow_ssh"
+    }
+
+}
