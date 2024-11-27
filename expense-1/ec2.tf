a08afeaeaecc252a3953af5419a8c85a531dd2bd
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2024-11-27 10:18:03 +0530

    some lines

--- expense-1/ec2.tf
+++ expense-1/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "terraform" {
     count = length(var.instance_names)
     ami = data.aws_ami.ami_info.id
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
-    instance_type = "t3.micro"
+    instance_type = var.instance_names[count.index] == "mysql" ? "t3.small" : "t3.micro"
     tags = merge(
         var.common_tags,
         {
@@ -33,6 +33,7 @@ resource "aws_security_group" "allow_ssh_terraform" {
         cidr_blocks      = var.ingress_cidr
         ipv6_cidr_blocks = ["::/0"]
     }
-    tags = var.tags
-
+    tags = {
+        Name = "alow_ssh"
+    }
 }
