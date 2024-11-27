69a505f4269d4a19ff386567d41068f625aad614
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2024-11-27 09:45:44 +0530

    some lines

--- expense-1/ec2.tf
+++ expense-1/ec2.tf
@@ -10,3 +10,29 @@ resource "aws_instance" "terraform" {
         }
     )    
 }
+
+resource "aws_security_group" "allow_ssh_terraform" {
+    name        = var.sg_name
+    description = var.sg_description
+
+    #usally we allow every thing in egress
+    # block
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    ingress {
+        from_port        = var.from_port
+        to_port          = var.to_port
+        protocol         = var.protocol
+        cidr_blocks      = var.ingress_cidr
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    tags = var.tags
+
+}
